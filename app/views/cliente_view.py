b12e7ffb4461e124682a0534930d5be83e6cc8b3
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2018-12-21 16:18:25 -0300

    10.04 - Renderizando formulário

--- app/views/cliente_view.py
+++ app/views/cliente_view.py
@@ -1,12 +1,19 @@
 from flask import render_template
+from app.forms import cliente_form
 
 from app import app
 
-@app.route("/ola", defaults={'nome': None})
-@app.route("/ola/<string:nome>")
-def teste(nome):
-    return render_template("clientes/teste.html", nome_usuario=nome)
+# @app.route("/ola", defaults={'nome': None})
+# @app.route("/ola/<string:nome>")
+# def teste(nome):
+#     return render_template("clientes/teste.html", nome_usuario=nome)
+#
+# @app.route("/oi")
+# def oi():
+#     return "Oi, mundo em Flask!!"
 
-@app.route("/oi")
-def oi():
-    return "Oi, mundo em Flask!!"
+@app.route("/cadastrar_cliente")
+def cadastrar_cliente():
+    form = cliente_form.ClienteForm()
+
+    return render_template("clientes/form.html", form=form)
