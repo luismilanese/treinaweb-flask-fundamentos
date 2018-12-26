6a854ed118e1bb17ea784dc0f8c2f9b3c80fc885
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2018-12-26 10:04:57 -0300

    11.01 - Persistindo dados do formulário no BD

--- app/views/cliente_view.py
+++ app/views/cliente_view.py
@@ -1,7 +1,10 @@
 from flask import render_template
 from app.forms import cliente_form
+from app import db
 
 from app import app
+from app.models import cliente_model
+
 
 # @app.route("/ola", defaults={'nome': None})
 # @app.route("/ola/<string:nome>")
@@ -12,8 +15,23 @@ from app import app
 # def oi():
 #     return "Oi, mundo em Flask!!"
 
-@app.route("/cadastrar_cliente")
+
+
+@app.route("/cadastrar_cliente", methods=["GET", "POST"])
 def cadastrar_cliente():
     form = cliente_form.ClienteForm()
+    if form.validate_on_submit():
+        nome = form.nome.data
+        email = form.email.data
+        data_nascimento = form.data_nascimento.data
+        profissao = form.profissao.data
+        sexo = form.sexo.data
+
+        cliente = cliente_model.Cliente(nome=nome, email=email, data_nascimento=data_nascimento, profissao=profissao, sexo=sexo)
+        try:
+            db.session.add(cliente)
+            db.session.commit()
+        except:
+            print("Cliente não cadastrado")
 
     return render_template("clientes/form.html", form=form)
